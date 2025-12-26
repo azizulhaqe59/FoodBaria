53ec47271b5a22b3a607a0e37bd8055638934193
unknown <taniacomputer@gmail.com>
2025-12-26 23:01:53 +0600

    next.congig update

--- next.config.js
+++ next.config.js
@@ -1,44 +1,20 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    swcPlugins: [],
-  },
+
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production' ? {
-      exclude: ['error', 'warn'], // Keep error and warn, remove log, info, debug, etc.
-    } : false,
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? { exclude: ["error", "warn"] }
+        : false,
   },
+
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '**', // allows all https domains
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**', // allows all https domains
-        pathname: '/**',
-      },
+    domains: [
+      "dev.foodbaria.com",
+      "web.foodbaria.com",
     ],
   },
 };
 
 module.exports = nextConfig;
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: [
-//       "bjorn66.com",
-//       "6ammart-test.6amdev.xyz",
-//       "192.168.50.168",
-//       "6ammart-dev.6amdev.xyz",
-//     ], // Add the domain here
-//   },
-// };
-//
-// module.exports = nextConfig;
